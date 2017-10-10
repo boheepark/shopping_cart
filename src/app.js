d7584847e4fe1bbac06b5098efe9b34e25b00856
Unknown <boheepark@yahoo.com>
2017-10-10 14:45:51 -0400

    initialized repo

---
+++ src/app.js
@@ -0,0 +1,33 @@
+"use strict"
+
+// REACT
+import React from 'react';
+import {render} from 'react-dom';
+import {Provider} from 'react-redux';
+
+import {applyMiddleware, createStore} from 'redux';
+import logger from 'redux-logger'
+
+// IMPORT COMBINED REDUCERS
+import reducers from './reducers/index';
+
+// IMPORT ACTIONS
+import {addToCart} from './actions/cartActions';
+import {postBooks, deleteBooks, updateBooks} from './actions/booksActions';
+
+// STEP 1 create the store
+const middleware = applyMiddleware(logger);
+const store = createStore(reducers, middleware);
+
+import BooksList from './components/pages/booksList';
+
+render(
+  <Provider store={store}>
+  <BooksList />
+  </Provider>, document.getElementById('app')
+);
+
+// POST a book
+// store.dispatch(postBooks(
+//
+// ));
---
+++ src/reducers/booksReducers.js
@@ -0,0 +1,61 @@
+"use strict"
+
+// BOOKS REDUCERS
+export function booksReducers(state={
+  books:
+  [{
+    id: 1,
+    title: 'this is the book title',
+    description: 'this is the book description',
+    price: 33.33
+  },
+  {id: 2,
+  title: 'this is the second book title',
+  description: 'this is the second book description',
+  price: 55}
+]}, action){
+  switch(action.type){
+    case "GET_BOOK":
+      return {...state, books:[...state.books]};
+      break;
+    case "POST_BOOK":
+      // let books = state.books.concat(action.payload);
+      // return {books};
+      return {books:[...state.books, ...action.payload]};
+      break;
+
+    case "DELETE_BOOK":
+      // Create a copy of the current array of books
+      const currentBookToDelete = [...state.books]
+      // Determine at which index in books array is the book to be deleted
+      const indexToDelete = currentBookToDelete.findIndex(
+        function(book){
+          return book.id === action.payload.id;
+        }
+      )
+      return {books: [...currentBookToDelete.slice(0, indexToDelete), ...currentBookToDelete.slice(indexToDelete + 1)]}
+      break;
+
+    case "UPDATE_BOOK":
+      // Create a copy of the current array of books
+      const currentBookToUpdate = [...state.books]
+      // Determine at which index in books array is the book to be deleted
+      const indexToUpdate = currentBookToUpdate.findIndex(
+        function(book){
+          return book.id === action.payload.id;
+        }
+      )
+
+      // Create a new book object with the new values and with the same array index of the item we want to replace. To achieve this we will use ...spread but we could use concat mothods too
+      const newBookToUpdate = {
+        ...currentBookToUpdate[indexToUpdate],
+        title: action.payload.title
+      }
+      // This Log has the purpose to show you how newBookToUpdate looks like
+      console.log("what is it newBookToUpdate", newBookToUpdate);
+      // use slice to remove the book at the specified index, replace with the new object and concatenate with the rest of items in the array
+      return {books: [...currentBookToUpdate.slice(0, indexToUpdate), newBookToUpdate, ...currentBookToUpdate.slice(indexToUpdate + 1)]}
+      break;
+  }
+  return state
+}
